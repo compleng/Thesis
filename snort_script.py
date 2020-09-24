734848943dde331b4439dc4bae5ea7b1eff1fd27
İlayda Gündoğdu <ilaydagundogdu95@gmail.com>
2020-09-25 01:10:36 +0300

    Create snort_script.py

---
+++ snort_script.py
@@ -0,0 +1,27 @@
+import subprocess
+import shutil
+import os
+year ="2013"
+for x in range(8):
+
+	for root, dirs, files in os.walk(year):
+	    for filename in files:
+		with open('/var/log/snort/alert', 'w') as fp: 
+	    		pass
+		shutil.copy("/var/log/snort/alert","/var/log/snort/old_alert")
+		#sudo snort -v -c /etc/snort/snort.conf -r filename -A full
+		subprocess.call(["snort", "-v", "-c", "/etc/snort/snort.conf", "-r", os.path.abspath(year + "/"+filename),"-A", "full"])
+		alert_file = open("/var/log/snort/alert",'r')	
+		output = alert_file.read()
+
+		output_file = open("Snort_Alerts", "a")
+		output_file.write("+++++++++++++++"+filename+"+++++++++++++++\n")
+		for line in output.splitlines():
+	    	  if "[**]" in line:
+			output_file.writelines(line + "\n")
+		output_file.write("-----------------------------------------------------------------\n\n\n\n")  
+
+		os.remove("/var/log/snort/alert")
+		os.remove("/var/log/snort/old_alert")
+
+	year = str(int(year)+1)
